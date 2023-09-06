f5de1265f805937aaa5d1777406249089067a2a5
beehnaaz <122270475+beehnaaz@users.noreply.github.com>
2023-09-06 19:08:18 +0330

    added css

--- src/Weather.js
+++ src/Weather.js
@@ -2,8 +2,52 @@ import React from "react";
 import "./Weather.css";
 export default function Weather() {
   return (
-    <div>
-      <h1 class="test">Hello Dear Behnaz</h1>
+    <div ClassName="container">
+      <div ClassName="Weather">
+        <form>
+          <div className="row">
+            <div className="col-6">
+              <input
+                type="search"
+                placeholder="Search for City..."
+                className="form-control"
+              />
+            </div>
+            <div className="col-6">
+              <input
+                type="submit"
+                value="search"
+                className="btn btn-primary w-10"
+              />
+            </div>
+          </div>
+        </form>
+        <div>
+          <h1>Dubai</h1>
+          <ul>
+            <li>Weather </li>
+            <li>Wednesday 18:00 </li>
+            <li>Mostly sunny</li>
+          </ul>
+        </div>
+
+        <div class="row">
+          <div class="col-6">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly sunny"
+            />
+            37°C
+          </div>
+          <div class="col-6">
+            <ul>
+              <li>Precipitation: 1% </li>
+              <li>Humidity: 62% </li>
+              <li>Wind: 14 mph</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
