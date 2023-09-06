9b29a2f9418e6a651767dc8ffa0431e5d3c55f85
beehnaaz <122270475+beehnaaz@users.noreply.github.com>
2023-09-06 16:39:59 +0330

    added css for Weather comp

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Weather.css";
 export default function Weather() {
   return (
     <div>
-      <h1>Hello Dear Behnaz</h1>
+      <h1 class="test">Hello Dear Behnaz</h1>
     </div>
   );
 }
